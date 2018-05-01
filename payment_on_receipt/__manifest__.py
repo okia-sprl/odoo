cba0fbb174fee774695e9d858add9447b2c4aa54
Sylvain Van Hoof (Okia SPRL) <sylvain@okia.be>
2018-05-01 23:08:06 +0200

    [ADD] Add the module payment_on_receipt

---
+++ payment_on_receipt/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Okia SPRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Payment on Receipt',
+    'category': 'Hidden',
+    'summary': '',
+    'version': '1.0',
+    'description': """
+Allow to pay on receipt for webshop.
+    """,
+    'author': 'Sylvain Van Hoof <sylvain@okia.be>',
+    'depends': ['payment'],
+    'data': [
+        'views/on_receipt.xml',
+        'data/payment_acquirer.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+}
---
+++ payment_on_receipt/models/payment_acquired.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Okia SPRL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+import logging
+import pprint
+
+from odoo import models, api, fields
+from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_compare
+
+_logger = logging.getLogger(__name__)
+
+
+class PaymentOnReceipt(models.Model):
+    _inherit = 'payment.acquirer'
+
+    provider = fields.Selection(
+        selection_add=[('on_receipt', 'On Receipt')])
+
+    @api.multi
+    def on_receipt_get_form_action_url(self):
+        return '/payment/transfer/feedback'
+
+
+class OnReceiptPaymentTransaction(models.Model):
+    _inherit = 'payment.transaction'
+
+    @api.model
+    def _on_receipt_form_get_tx_from_data(self, data):
+        reference = data.get('reference')
+
+        tx_ids = self.search([('reference', '=', reference)])
+
+        if not tx_ids or len(tx_ids) > 1:
+            error_msg = \
+                'received data for reference %s' % (pprint.pformat(reference))
+            if not tx_ids:
+                error_msg += '; no order found'
+            else:
+                error_msg += '; multiple order found'
+            _logger.error(error_msg)
+            raise UserError(error_msg)
+
+        return tx_ids
+
+    @api.model
+    def _on_receipt_form_get_invalid_parameters(self, data):
+        invalid_parameters = []
+
+        if float_compare(float(data.get('amount', '0.0')),
+                         self.amount, 2) != 0:
+            invalid_parameters.append(
+                ('amount', data.get('amount'), '%.2f' % self.amount))
+        if data.get('currency') != self.currency_id.name:
+            invalid_parameters.append(
+                ('currency', data.get('currency'), self.currency_id.name))
+
+        return invalid_parameters
+
+    @api.model
+    def _on_receipt_form_validate(self, data):
+        _logger.info('Validated payment on receipt for tx %s: set as pending' %
+                     self.reference)
+        return self.write({'state': 'pending'})
