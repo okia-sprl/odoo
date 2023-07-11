c994dc129b330a25c2f49767901bf3fcb89b8c76
Sylvain Van Hoof <sylvain@okia.be>
2023-07-11 23:12:10 +0200

    [FIX] Replace the method product_id_change by _compute_product_uom

--- sale_uom/models/sale_order_line.py
+++ sale_uom/models/sale_order_line.py
@@ -4,17 +4,10 @@ from odoo import api, models
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    @api.onchange("product_id")
-    def product_id_change(self):
-        self.ensure_one()
+    @api.depends("product_id")
+    def _compute_product_uom(self):
+        super()._compute_product_uom()
 
-        result = super().product_id_change()
-
-        if not self.product_id:
-            return result
-
-        if self.product_id.uom_so_id:
-            self.product_uom = self.product_id.uom_so_id.id
-            self.product_uom_change()
-
-        return result
+        for line in self:
+            if line.product_id.uom_so_id:
+                line.product_uom = line.product_id.uom_so_id
