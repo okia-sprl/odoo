5123c6fcafdaa046d5fc845eab99cd2290172bfc
Sylvain Van Hoof <sylvain@okia.be>
2023-07-20 09:43:55 +0200

    [REM] Remove old and useless copyright

--- payment_on_receipt/models/payment_acquired.py
+++ payment_on_receipt/models/payment_acquired.py
@@ -1,5 +1,3 @@
-# Copyright 2017 Okia SPRL
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import logging
 import pprint
 
--- sale_category_limitation/__manifest__.py
+++ sale_category_limitation/__manifest__.py
@@ -1,5 +1,3 @@
-# Copyright 2019 Okia SPRL
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Sale limitation by product categories",
     "author": "Okia SRL",
